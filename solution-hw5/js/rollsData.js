349e1d2f57d24a38532d98d0aee9da5b4a4d5ab2
qilin3805 <147442831+qilin3805@users.noreply.github.com>
2024-02-24 14:12:31 -0500

    Homework5 Add & Delete items from Cart.

---
+++ solution-hw5/js/rollsData.js
@@ -0,0 +1,42 @@
+const rolls = {
+    "Original": {
+        "basePrice": 2.49,
+        "imageFile": "original-cinnamon-roll.jpg"
+    },
+    "Apple": {
+        "basePrice": 3.49,
+        "imageFile": "apple-cinnamon-roll.jpg"
+    },
+    "Raisin": {
+        "basePrice": 2.99,
+        "imageFile": "raisin-cinnamon-roll.jpg"
+    },
+    "Walnut": {
+        "basePrice": 3.49,
+        "imageFile": "walnut-cinnamon-roll.jpg"
+    },
+    "Double-Chocolate": {
+        "basePrice": 3.99,
+        "imageFile": "double-chocolate-cinnamon-roll.jpg"
+    },
+    "Strawberry": {
+        "basePrice": 3.99,
+        "imageFile": "strawberry-cinnamon-roll.jpg"
+    }    
+};
+
+
+// A list of price adaptations object based on user selections.
+const allGlazing = {
+    'Keep original': 0.00,
+    'Sugar milk': 0.00,
+    'Vanilla milk': 0.50,
+    'Double chocolate': 1.50
+};
+
+const allPackSize = {
+    1: 1,
+    3: 3,
+    6: 5,
+    12: 10
+};
---
+++ solution-hw5/js/shopping-cart.js
@@ -0,0 +1,115 @@
+const rollcardSet = new Set();
+
+class Roll {
+    constructor(rollType, rollGlazing, packSize, rollPrice) {
+        this.type = rollType;
+        this.glazing = rollGlazing;
+        this.size = packSize;
+        this.basePrice = rollPrice;
+    }
+}
+
+const cartPrice = document.querySelector('#cart-price');
+
+/* ------------------------------------------------------------------------- */
+
+// Initialize the shopping cart.
+// new Roll objects and add to cart set.
+function addNewCard(rollType, rollGlazing, packSize, rollPrice) {
+    const rollcard = new Roll(rollType, rollGlazing, packSize, rollPrice);
+
+    const rollInfo = rolls[rollType];
+    rollcard.rollImageURL = "../assets/products/" + rollInfo.imageFile;
+    rollcard.calculatedPrice = calculatePrice(rollcard);
+    rollcard.element = null;
+    
+    rollcardSet.add(rollcard);
+    return rollcard;
+}
+
+addNewCard("Original", "Sugar milk", 1, 2.49);
+addNewCard("Walnut", "Vanilla milk", 12, 3.49);
+addNewCard("Raisin", "Sugar milk", 3, 2.99);
+addNewCard("Apple", "Keep original", 3, 3.49);
+
+// appends DOM elements to the shopping cart page.
+for (const rollcard of rollcardSet) {
+    createElement(rollcard);
+}
+
+// grab a reference to the notecard template and update
+function createElement(rollcard) {
+    const template = document.querySelector('#rollcart-template');
+    const clone = template.content.cloneNode(true);
+    rollcard.element = clone.querySelector('.rollcard');
+  
+    const rollcardListElement = document.querySelector('#rollcard-list');
+    rollcardListElement.append(rollcard.element);
+
+    updateElement(rollcard);
+}
+
+// update image, type, glazing, packsize in the page
+function updateElement(rollcard) {
+    const rollImageElement = rollcard.element.querySelector('.card-url');
+    const rollTypeElement = rollcard.element.querySelector('.card-title');
+	const rollGlazingElement = rollcard.element.querySelector('.card-glaze');
+	const packSizeElement = rollcard.element.querySelector('.card-size');
+    const rollPriceElement = rollcard.element.querySelector('.card-price');
+    const removeLink = rollcard.element.querySelector('.remove-link');
+
+    // The correct picture for the roll.
+    rollImageElement.src = rollcard.rollImageURL;
+    // The name of the item.
+    rollTypeElement.innerText = rollcard.type + " Cinnamon Roll";
+    // The glazing for the item.
+    rollGlazingElement.innerText = `Glazing: ${rollcard.glazing}`;
+    // The pack size.
+    packSizeElement.innerText = `Pack Size: ${rollcard.size}`;
+    // The item price you calculate.
+    rollPriceElement.innerText = `$${rollcard.calculatedPrice}`;
+    // A “remove” link for the item.
+    removeLink.addEventListener('click', () => {
+        deleteCard(rollcard);
+    });
+}
+
+/* ------------------------------------------------------------------------- */
+
+// Update when click on the remove link.
+// remove one knid of item
+function deleteCard(rollcard) {
+    // step1 remove element in HTML
+    rollcard.element.remove();
+    // step2 remove instance in set
+    rollcardSet.delete(rollcard);
+    // step3 update total price in cart
+    sumPrice();
+}
+
+// calculate total price of the item daynamically
+function calculatePrice(rollcard) {
+    let calculatedPrice = 0;
+    // Retrieve the object at the index specified by the select's value
+    let glazingToCompute = allGlazing[rollcard.glazing];
+    let packSizeToCompute = allPackSize[rollcard.size];
+    console.log(rollcard.size);
+    
+    // Compute total price and update the UI
+    calculatedPrice = ((rollcard.basePrice + glazingToCompute) * packSizeToCompute).toFixed(2);
+    return calculatedPrice;
+}
+
+// update totoal price of the cart daynamically
+function sumPrice() {
+    let totalPrice = 0;
+    for (const rollcard of rollcardSet) {
+        totalPrice += parseFloat(rollcard.calculatedPrice);
+    }
+    console.log(totalPrice);
+    cartPrice.innerText = `$${totalPrice}`;
+
+}
+
+// load the cart price
+sumPrice();
